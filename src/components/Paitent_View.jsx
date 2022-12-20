c47ef322a995ce630720443053b71cb212d43be8
sadanandkumar <sadanandkr9686@gmail.com>
2022-12-20 18:24:50 +0530

    report

--- src/components/Paitent_View.jsx
+++ src/components/Paitent_View.jsx
@@ -2,16 +2,125 @@ import { height } from '@mui/system';
 import React from 'react'
 import BorderColorIcon from '@mui/icons-material/BorderColor';
 
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import TextField from '@mui/material/TextField';
+import { NavItem } from 'react-bootstrap';
+
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
+
+
 
 
 const Patient = () => {
-    const medicien = [
-        {
-            img: "https://5.imimg.com/data5/SELLER/Default/2021/5/MJ/PD/DW/39688399/combiflam-tablet-500x500.jpg",
-            name: "Combiflame",
-            actions: ""
+    // const medicien = [
+    //     {
+    //         img: "https://5.imimg.com/data5/SELLER/Default/2021/5/MJ/PD/DW/39688399/combiflam-tablet-500x500.jpg",
+    //         name: "Combiflame",
+    //         actions: ""
+    //     }
+    // ]
+
+
+    const [state, setState] = React.useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
         }
-    ]
+
+        setState({ ...state, [anchor]: open });
+    };
+
+    const list = (anchor) => (
+        <Box
+            sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 400 }}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        > Dosages
+
+            <Box
+                component="form"
+                sx={{
+                    '& .MuiTextField-root': { m: 1, width: '20ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <TextField
+                    id="outlined-number"
+                    label="Gap"
+                    type="number"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+                <TextField
+                    required
+                    id="filled-required"
+                    label="Start date"
+                    defaultValue="dd/mm/yyy"
+                    variant="filled"
+                />
+                <TextField
+                    required
+                    id="filled-required"
+                    label="End date"
+                    defaultValue="dd/mm/yyy"
+                    variant="filled"
+                />
+                <Divider variant="fullWidth" light={true}>--- </Divider>
+                <FormControl>
+                    <FormLabel id="demo-row-radio-buttons-group-label">Timing</FormLabel>
+                    <RadioGroup
+                        row
+                        aria-labelledby="demo-row-radio-buttons-group-label"
+                        name="row-radio-buttons-group"
+                    >
+                        <FormControlLabel value="female" control={<Radio />} label="Morning" />
+                        <FormControlLabel value="male" control={<Radio />} label="Noon" />
+                        <FormControlLabel value="other" control={<Radio />} label="Evening" />
+
+                    </RadioGroup>
+                </FormControl>
+            </Box>
+
+            {/* <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem key={text} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List> */}
+            {/* <Divider /> */}
+        </Box>
+    );
+
     return (
         <div class="container-fluid">
 
@@ -49,12 +158,21 @@ const Patient = () => {
                                     <th scope="row">1</th>
                                     <td> <img src="https://5.imimg.com/data5/SELLER/Default/2021/5/MJ/PD/DW/39688399/combiflam-tablet-500x500.jpg" alt="Avatar" class="avatar" /></td>
                                     <td>combiflam</td>
-                                    <td>
-                                        <button className='button mx-2'
-                                            onClick={() => alert('hello')} > view</button>
-                                              <button className='button mx-2'
-                                            onClick={() => alert('hello')} ><BorderColorIcon/></button>
-                                    </td>
+                                    {['left'].map((anchor) => (
+                                        <td key={anchor}>
+                                            <button className='button mx-2'
+                                                onClick={toggleDrawer(anchor, true)} >View</button>
+                                            <Drawer
+                                                anchor={anchor}
+                                                open={state[anchor]}
+                                                onClose={toggleDrawer(anchor, false)}
+                                            >
+                                                {list(anchor)}
+                                            </Drawer>
+                                            <button className='button mx-2'
+                                                onClick={() => alert('hello')} ><BorderColorIcon /></button>
+                                        </td>
+                                    ))}
                                 </tr>
                             </tbody>
                         </table>
@@ -63,7 +181,23 @@ const Patient = () => {
 
 
             </div>
+
+            <div>
+                {/* {['left', 'right', 'top', 'bottom'].map((anchor) => (
+                    <React.Fragment key={anchor}>
+                        <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+                        <Drawer
+                            anchor={anchor}
+                            open={state[anchor]}
+                            onClose={toggleDrawer(anchor, false)}
+                        >
+                            {list(anchor)}
+                        </Drawer>
+                    </React.Fragment>
+                ))} */}
+            </div>
         </div>
+
     );
 }
 
